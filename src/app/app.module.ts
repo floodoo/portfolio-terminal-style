631d15e115334cd63c0f8c1e2a143338877a8606
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-06-21 00:04:47 +0200

    add terminal home text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalNavbarComponent } from './terminal-navbar/terminal-navbar.component';
+import { TerminalHomeComponent } from './terminal-home/terminal-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TerminalNavbarComponent
+    TerminalNavbarComponent,
+    TerminalHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/terminal-home/terminal-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal-home',
+  templateUrl: './terminal-home.component.html',
+  styleUrls: ['./terminal-home.component.scss']
+})
+export class TerminalHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
